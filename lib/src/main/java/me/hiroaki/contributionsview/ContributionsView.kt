ab6ebeedb5fadd69db87db7eb9188ffc60a9448c
Yuki Nakakura <hiroaki.dev@gmail.com>
2018-07-06 22:14:58 +0900

    左上の最初の四角を描画

--- lib/src/main/java/me/hiroaki/contributionsview/ContributionsView.kt
+++ lib/src/main/java/me/hiroaki/contributionsview/ContributionsView.kt
@@ -12,6 +12,17 @@ import android.view.View
 
 
 class ContributionsView : View {
+
+    private val squareSize = 12
+    private val squarePaint = Paint().apply {
+        color = Color.argb(255, 255, 0, 255)
+        style = Paint.Style.FILL_AND_STROKE
+    }
+    private val squarePaddingStart = 2
+    private val squarePaddingEnd = 2
+    private  val squarePaddingTop = 2
+    private val squarePaddingBottom = 2
+
     private companion object {
         val TAG = ContributionsView::class.java.simpleName
     }
@@ -29,16 +40,23 @@ class ContributionsView : View {
     @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
         val dp = resources.displayMetrics.density
+        var x = 0f
+        var y = 0f
+
         Log.d(TAG, "dp = $dp")
         Log.d(TAG, "canvasSize: width = ${canvas.width}px, height = ${canvas.height}px")
         Log.d(TAG, "canvasSize: width = ${canvas.width / dp}dp, height = ${canvas.height / dp}dp")
-        val paint = Paint()
-        paint.color = Color.argb(255, 255, 0, 255)
-        paint.style = Paint.Style.FILL_AND_STROKE
+
 
         // drawRectを使って矩形を描画する、引数に座標を設定
         // (x1,y1,x2,y2,paint) 左上の座標(x1,y1), 右下の座標(x2,y2)
-        canvas.drawRect(300f, 300f, 600f, 600f, paint)
+        canvas.drawRect(
+                x + squarePaddingStart * dp,
+                y + squarePaddingTop * dp,
+                x + (squarePaddingStart + squareSize + squarePaddingEnd) * dp,
+                y + (squarePaddingTop + squareSize + squarePaddingBottom) * dp,
+                squarePaint
+        )
         Log.d(TAG, "drawSize: width = ${300f / dp}dp, height = ${300f / dp}dp")
     }
 }
